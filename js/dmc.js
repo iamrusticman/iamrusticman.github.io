5ea88001496cb8ab789d978caa450ea05a2a8e5a
iamrusticman <iamrusticman@gmail.com>
2017-05-30 19:41:53 +0200

    Scroll mechanism

--- js/dmc.js
+++ js/dmc.js
@@ -1,6 +1,7 @@
 $(document).ready(function()
 {
     window.tracks = $(".track").toArray().reverse();
+    currentVideoPlayer = 1;
 });
 
 function startChartTour()
@@ -13,14 +14,36 @@ function startChartTour()
 function playTrack(index)
 {
     clearPlayer();
+    scrollPage(index);
     setTimeout(initPlayer(index), 0);
+}
+
+function scrollPage(index)
+{
+    window.scrollTo(0,document.body.scrollHeight - 50 - 60 * (index + 1));
 }
 
 function clearPlayer()
 {
-    $("#videoPlayer").hide();
     $(".playerLabel").hide();
-    videoPlayer = $("#videoPlayer")[0];
+}
+
+function switchPlayer(index)
+{
+    playerHeight = ((window.tracks.length - index) * 60) + 25;
+    playerContainer = $(".playerContainer")[0];
+    playerContainer.style["margin-top"] = playerHeight;
+    if (document.body.scrollHeight < playerHeight + 300) {
+        window.scrollTo(0,document.body.scrollHeight);
+    }
+    else {
+        window.scrollTo(0,document.body.scrollHeight - 50 - 60 * (index + 1));
+    }
+    $("#videoPlayer" + currentVideoPlayer).show();
+    currentVideoPlayer = currentVideoPlayer === 1 ? 2 : 1;
+    console.log("Now currentvideoplayer is " + currentVideoPlayer);
+    $("#videoPlayer" + currentVideoPlayer).hide();
+    videoPlayer = $("#videoPlayer" + currentVideoPlayer)[0];
     videoPlayer["src"] = "about:blank";
 }
 
@@ -42,26 +65,27 @@ function initPlayer(index)
     videoUrl = track.getAttribute("data-videourl");
     videoDuration = parseInt(track.getAttribute("data-videoduration"));
     videoDuration = videoDuration === NaN ? 18 : videoDuration;
-    videoPlayer = $("#videoPlayer")[0];
+    videoPlayer = $("#videoPlayer" + currentVideoPlayer)[0];
     videoPlayer["src"] = videoUrl;
 
     videoPlayer.onload = function() 
     {
-        setTimeout(function() {
-            $("#videoPlayer").show();
+        if (videoPlayer["src"] === "about:blank") return;
+        console.log("onload");
 
-            if (index < window.tracks.length) 
-            {
-                setTimeout(function() { 
-                    $(track).removeClass("current"); 
+        setTimeout(function() {
+            switchPlayer(index);
+            setTimeout(function() { 
+                $(track).removeClass("current"); 
+                if (index < window.tracks.length) {
                     playTrack(index + 1); 
-                }, videoDuration * 1000);
-            }
+                }
+            }, videoDuration * 1000);
         }, 1500);
 
         setTimeout(function() {
             showTrackLabel(index);
-        }, 3000);
+        }, 2500);
     }
 }
 
